88a5430462b621d43c6f95f05edaed6dfcc2d87f
hotfireeagle <hotfireeagle@163.com>
2022-12-04 18:14:43 +0800

    repl

---
+++ repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"jpg/lexer"
+	"jpg/reader"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		lr := reader.New(reader.TextMode, line)
+		l := lexer.New(lr)
+
+		for tok := l.NextToken(); !tok.IsEof(); tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
