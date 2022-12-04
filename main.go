594c296d76bab23e9182b3d7dea0451c90dde827
hotfireeagle <hotfireeagle@163.com>
2022-12-04 17:56:53 +0800

    feat: 词法分析基本完成

--- main.go
+++ main.go
@@ -2,21 +2,20 @@ package main
 
 import (
 	"fmt"
-	"jpg/languagespec"
+	"jpg/lexer"
+	"jpg/reader"
 )
 
 func main() {
-	var str string = `"w"`
-	fmt.Println("check is string", languagespec.StringDoubleDFA.Match(str))
 	// codeString := "sjdsjkdjskdjskdjkskdsjdjsjk"
-	// filePath := "/Users/smallhai/learn/gitRepo/jpg/test.js"
-	// reader := reader.New(reader.FileMode, filePath)
-	// lexer := lexer.New(reader)
+	filePath := "/Users/smallhai/learn/gitRepo/jpg/test.js"
+	reader := reader.New(reader.FileMode, filePath)
+	lexer := lexer.New(reader)
 
-	// token := lexer.NextToken()
+	token := lexer.NextToken()
 
-	// for !token.IsEof() {
-	// 	fmt.Println("token", token.Literal, token.Type)
-	// 	token = lexer.NextToken()
-	// }
+	for !token.IsEof() {
+		fmt.Println("token", token.Literal, token.Type)
+		token = lexer.NextToken()
+	}
 }
--- test.js
+++ test.js
@@ -1,1 +1,7 @@
 "232"
+var a = 12
+function fun() {
+  const c = 21
+  return c
+}
+const s = fun()
