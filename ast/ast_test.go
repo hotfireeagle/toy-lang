a85686a1421e8d7faf38e4e7afbcfd9564c0b8fb
hotfireeagle <hotfireeagle@163.com>
2022-12-18 22:45:27 +0800

    String

---
+++ ast/ast_test.go
@@ -0,0 +1,28 @@
+package ast
+
+import (
+	"jpg/tokentype"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: tokentype.New(tokentype.LET, "let"),
+				Name: &Identifier{
+					Token: tokentype.New(tokentype.IDENTIFIER, "myVar"),
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: tokentype.New(tokentype.IDENTIFIER, "anotherVar"),
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong, got=%q", program.String())
+	}
+}
